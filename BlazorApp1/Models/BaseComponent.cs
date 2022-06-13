49b6d0f39c89361853ab0cefae2af01ed74e6941
Dat Truong <dtruong@mfah.org>
2022-06-13 18:40:49 -0500

    init

---
+++ BlazorApp1/Models/BaseComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1.Models
+{
+    public class BaseComponent
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+
+        public Boolean Active { get; set; }
+
+        public string Content { get; set; }
+    }
+}
