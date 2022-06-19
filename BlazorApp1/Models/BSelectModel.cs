2dcc284c3d8d32182613e25d95405c6aa5129dc9
Dat Truong <dtruong@mfah.org>
2022-06-19 14:41:50 -0500

    Add BSearch and Select

---
+++ BlazorApp1/Models/BSelectModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Models
+{
+    public class BSelectModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PlaceHolder { get; set; }
+
+        public IEnumerable<BSelectOption> Options { get; set; }
+    }
+    public class BSelectOption 
+    { 
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+}
